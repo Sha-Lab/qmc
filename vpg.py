81b1ab5bcd07bc423832e6997bfcaa8b3f622b80
lchenat <lchenat@connect.ust.hk>
2019-06-17 00:53:12 -0700

    search vpg

--- vpg.py
+++ vpg.py
@@ -6,17 +6,19 @@ from ipdb import launch_ipdb_on_exception
 
 from envs import *
 from models import get_mlp, GaussianPolicy
-from utils import tensor
+from utils import tensor, set_seed
 
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_trajs', type=int, default=500)
-    parser.add_argument('--n_iters', type=int, default=3000)
+    parser.add_argument('--n_iters', type=int, default=6000)
     parser.add_argument('--horizon', type=int, default=100)
     parser.add_argument('--discount', type=float, default=0.99)
     parser.add_argument('-lr', type=float, default=0.01)
     parser.add_argument('--hidden_sizes', type=int, nargs='+', default=[8])
+    parser.add_argument('--save_fn', type=str, default=None)
+    parser.add_argument('--seed', type=int, default=0)
     return parser.parse_args(args)
 
 def reinforce_loss(states, actions, returns, policy):
@@ -25,9 +27,11 @@ def reinforce_loss(states, actions, returns, policy):
 
 def main(args=None):
     args = parse_args(args)
+    iter_returns = []
+    set_seed(args.seed)
     env = CartPoleContinuousEnv()
     state_dim, action_dim = env.observation_space.shape[0], env.action_space.shape[0] 
-    mean_network = get_mlp((state_dim,)+args.hidden_sizes+(action_dim,), gate=nn.ReLU, output_gate=nn.Tanh)
+    mean_network = get_mlp((state_dim,)+tuple(args.hidden_sizes)+(action_dim,), gate=nn.ReLU, output_gate=nn.Tanh)
     policy = GaussianPolicy(state_dim, action_dim, mean_network)
     optim = torch.optim.SGD(policy.parameters(), args.lr)
     for _ in range(args.n_iters):
@@ -66,7 +70,13 @@ def main(args=None):
         loss = reinforce_loss(observations, actions, returns, policy)
         loss.backward()
         optim.step()
-        print('Average Return:', np.mean([sum(p["rewards"]) for p in paths]))
+        iter_returns.append(np.mean([sum(p["rewards"]) for p in paths]))
+        print('Average Return:', iter_returns[-1])
+    if args.save_fn:
+        Path(args.save_fn).parent.mkdir(parents=True, exist_ok=True)
+        with open(args.save_fn) as f:
+            for ret in iter_returns:
+                f.write('{}\n'.format(ret))
 
 if __name__ == "__main__":
     with launch_ipdb_on_exception():
